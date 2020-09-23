2bc65f4341c9f637bbd351b26b1761856d89f376
hector galicia milla <hectormilla8@gmail.com>
2020-09-23 17:36:32 -0400

    function that will clear input fields and set focus back to default

--- starter/app.js
+++ starter/app.js
@@ -95,6 +95,18 @@ var uiController = (function () {
       // Insert the HTML into the DOM
       document.querySelector(element).insertAdjacentHTML("beforeend", newHtml);
     },
+    clearFields: function () {
+      var fields, fieldsArray;
+      fields = document.querySelectorAll(
+        DOMStrings.inputDescription + "," + DOMStrings.inputValue
+      );
+      fieldsArray = Array.prototype.slice.call(fields);
+
+      fieldsArray.forEach(function (current, index, array) {
+        current.value = "";
+      });
+      fieldsArray[0].focus(2);
+    },
   };
 })();
 
@@ -120,8 +132,10 @@ var controller = (function (budgetCtrl, uiCtrl) {
     newItem = budgetCtrl.addItem(input.type, input.description, input.value);
     // 3. Add the item to the UI
     uiCtrl.addListItem(newItem, input.type);
-    // 4. Calculate the budget
-    // 5. Display the budget on the UI
+    // 4. Clear the fields
+    uiCtrl.clearFields();
+    // 5. Calculate the budget
+    // 6. Display the budget on the UI
   };
   return {
     init: function () {

a63dde0c11aaba3741d17f8567766456a36ae699
hector galicia milla <hectormilla8@gmail.com>
2020-09-23 15:29:52 -0400

    added function that will create and add new items to data from any module

--- starter/app.js
+++ starter/app.js
@@ -10,9 +10,7 @@ var budgetController = (function () {
     this.description = description;
     this.value = value;
   };
-  var allExpenses = [];
-  var allIncome = [];
-  var totalExpenses;
+
   var data = {
     allItems: {
       exp: [],
@@ -20,7 +18,33 @@ var budgetController = (function () {
     },
     totals: {
       exp: 0,
-      inc: 9,
+      inc: 0,
+    },
+  };
+  return {
+    addItem: function (type, des, val) {
+      var newItem, ID;
+      // Create new ID
+      if (data.allItems[type].length > 0) {
+        ID = data.allItems[type][data.allItems[type].length - 1].id + 1;
+      } else {
+        ID = 0;
+      }
+
+      // Create new item based on 'inc' or 'exp' type
+      if (type === "exp") {
+        newItem = new Expense(ID, des, val);
+      } else if (type === "inc") {
+        newItem = new Income(ID, des, val);
+      }
+      // Push it into our data structure
+      data.allItems[type].push(newItem);
+
+      // Return new element
+      return newItem;
+    },
+    testing: function () {
+      console.log(data);
     },
   };
 })();
@@ -62,13 +86,14 @@ var controller = (function (budgetCtrl, uiCtrl) {
   };
 
   var ctrlAddItem = function () {
+    var input, newItem;
     // 1. Get the filled input data
-    var input = uiCtrl.getInput();
+    input = uiCtrl.getInput();
     // 2. Add the item to the budget controller
+    newItem = budgetCtrl.addItem(input.type, input.description, input.value);
     // 3. Add the item to the UI
     // 4. Calculate the budget
     // 5. Display the budget on the UI
-    console.log(input);
   };
   return {
     init: function () {

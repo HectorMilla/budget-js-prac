e26451c81f18141ab43d3b208d66d091c23a0b3b
hector galicia milla <hectormilla8@gmail.com>
2020-09-24 20:49:49 -0400

    added delete item function to delete item from data on button click

--- starter/app.js
+++ starter/app.js
@@ -53,6 +53,18 @@ var budgetController = (function () {
       // Return new element
       return newItem;
     },
+    deleteItem: function (type, id) {
+      var ids, index;
+
+      ids = data.allItems[type].map(function (current) {
+        return current.id;
+      });
+
+      index = ids.indexOf(id);
+      if (index !== -1) {
+        data.allItems[type].splice(index, 1);
+      }
+    },
     caculateBudget: function () {
       // calculate total income and expenses
       calculateTotal("exp");
@@ -206,8 +218,13 @@ var controller = (function (budgetCtrl, uiCtrl) {
     if (itemID) {
       splitID = itemID.split("-");
       type = splitID[0];
-      ID = splitID[1];
+      ID = parseInt(splitID[1]);
     }
+
+    // 1. delete the item from the data structure
+    budgetCtrl.deleteItem(type, ID);
+    // 2. Delete the item from the UI
+    // 3. Update and show the new budget
   };
   return {
     init: function () {
